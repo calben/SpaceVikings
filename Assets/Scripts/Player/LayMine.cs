1e0ff93e137ae297682024ba1a5b73f346d14f62
gglang <gerald.lang@mail.mcgill.ca>
2015-03-28 18:18:02 -0400

    Lay a mine!

---
+++ Assets/Scripts/Player/LayMine.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LayMine : MonoBehaviour {
+	public Transform mine;
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			print("space key was pressed");
+			Instantiate(mine, transform.position, Quaternion.identity);
+
+		}
+	}
+}
