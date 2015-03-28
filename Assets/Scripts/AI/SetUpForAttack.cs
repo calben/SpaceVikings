05d7c41bc91ffd979da43796ccead87d0bb23e5d
Calem Bendell <calem.j.bendell@gmail.com>
2015-03-28 16:22:51 -0400

    initialised

---
+++ Assets/Scripts/AI/SetUpForAttack.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetUpForAttack : MonoBehaviour
+{
+
+  public GameObject agent;
+  public Vector3 goalPosition;
+  public Quaternion goalRotation;
+  public float preferredShootingDistanceFromPlayer = 10.0f;
+  public float preferredShootAngleFromPlayer = 90.0f;
+
+  void Start()
+  {
+    agent = this.gameObject.transform.parent.gameObject;
+  }
+
+  void Update()
+  {
+
+  }
+
+  void OnCollisionStay(Collision collisionInfo)
+  {
+    foreach (Collision col in collisionInfo)
+    {
+      GameObject obj = col.collider.gameObject;
+      if (obj.tag == "Player")
+      {
+        this.goalPosition = MakeGoalPositionFromPlayerPosition(obj.transform.position);
+        break;
+      }
+    }
+  }
+
+  Vector3 MakeGoalPositionFromPlayerPosition(Vector3 playerPosition)
+  {
+    Vector3 v = Vector3.zero;
+    return v;
+  }
+
+}
---
+++ Assets/Scripts/Map/GenerateRandomMapObjects.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GenerateRandomMapObjects : MonoBehaviour
+{
+
+  public GameObject a;
+  public GameObject b;
+  public GameObject c;
+  public GameObject d;
+  public int numberOfCubes;
+  public int min, max;
+  public string layer;
+  public Color cubecolor;
+
+  public void PlaceCubes()
+  {
+    for (int i = 0; i < numberOfCubes; i++)
+    {
+      GameObject aobj = (GameObject)Instantiate(a, GeneratePosition(), GenerateQuaternion());
+      GameObject bobj = (GameObject)Instantiate(b, GeneratePosition(), GenerateQuaternion());
+      GameObject cobj = (GameObject)Instantiate(c, GeneratePosition(), GenerateQuaternion());
+      GameObject dobj = (GameObject)Instantiate(d, GeneratePosition(), GenerateQuaternion());
+      GameObject[] objs = new GameObject[] { aobj, bobj, cobj, dobj };
+      foreach (GameObject obj in objs)
+      {
+        obj.AddComponent<MeshCollider>();
+        obj.GetComponent<MeshCollider>().convex = true;
+        obj.AddComponent<Rigidbody>();
+        obj.GetComponent<Rigidbody>().mass = 5;
+      }
+    }
+  }
+
+  Vector3 GeneratePosition()
+  {
+    int x, y, z;
+    x = UnityEngine.Random.Range(min, max);
+    y = UnityEngine.Random.Range(min, max);
+    z = UnityEngine.Random.Range(min, max);
+    return new Vector3(x, y, z);
+  }
+
+  Quaternion GenerateQuaternion()
+  {
+    Quaternion quat = new Quaternion();
+    quat.w = UnityEngine.Random.Range(0, 180);
+    quat.x = UnityEngine.Random.Range(0, 180);
+    quat.y = UnityEngine.Random.Range(0, 180);
+    quat.z = UnityEngine.Random.Range(0, 180);
+    return quat;
+  }
+
+  void Awake()
+  {
+    PlaceCubes();
+  }
+
+}
