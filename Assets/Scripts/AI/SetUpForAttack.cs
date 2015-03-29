eea0d6fecf582ad017b5990555242895dfffef40
Shalon Liu <shalon.liu@gmail.com>
2015-03-29 01:40:20 -0400

    AI moving code

--- Assets/Scripts/AI/SetUpForAttack.cs
+++ Assets/Scripts/AI/SetUpForAttack.cs
@@ -27,22 +27,57 @@ public class SetUpForAttack : MonoBehaviour
       GameObject obj = col.collider.gameObject;
       if (obj.tag == "Player")
       {
-        this.goalPosition = MakeGoalPositionFromPlayerPosition(obj.transform.position, obj.transform.forward);
-        this.goalForward = MakeGoalForwardFromPlayerPosition(obj.transform.position, obj.transform.forward);
+        this.goalPosition = MakeGoalPositionFromPlayerPosition(obj.transform.position);
+        this.goalForward = MakeGoalForwardFromPlayerPosition(obj.transform.position);
         break;
       }
     }
   }
 
-  Vector3 MakeGoalPositionFromPlayerPosition(Vector3 playerPosition, Vector3 playerForward)
+  Vector3 MakeGoalPositionFromPlayerPosition(Vector3 playerPosition)
   {
-    Vector3 v = Vector3.zero;
-    return v;
+	Vector3 diff = playerPosition - this.gameObject.transform.position;
+	if (diff.magnitude < 25) {
+		return this.gameObject.transform.position;
+	} else {
+		Vector3 goal = playerPosition + diff.normalized * (diff.magnitude - 25);
+		return goal;
+	}
   }
 
-  Vector3 MakeGoalForwardFromPlayerPosition(Vector3 playerPosition, Vector3 playerForward)
+  Vector3 MakeGoalForwardFromPlayerPosition(Vector3 playerPosition)
   {
-    Vector3 v = Vector3.zero;
+	float rotation_x = 0;
+	float rotation_y = 0;
+	float rotation_z = 0;
+	Vector3 diff = playerPosition - this.goalPosition;
+	float cosphi = Vector3.Dot (this.gameObject.transform.up,diff.normalized, diff.normalized);
+	float costheta = Vector3.Dot (this.gameObject.transform.forward,diff.normalized);
+	if (Math.Abs (cosphi) > .5) {
+		bool right = diff.z > 0;
+		if (cosphi > 0 ){
+			rotation_x = Mathf.PI/3 - Mathf.Acos(cosphi);
+		}
+		else{
+			rotation_x = Mathf.PI/3*2 - Mathf.Acos(cosphi);
+		}
+		if (right){
+			rotation_x = -rotation_x;
+		}
+	}
+	if (Math.Abs (costheta) > .5) {
+		bool right = diff.z > 0;
+		if (costheta > 0 ){
+			rotation_y = Mathf.PI/3 - Mathf.Acos(costheta);
+		}
+		else{
+			rotation_y = Mathf.PI/3*2 - Mathf.Acos(costheta);
+		}
+		if (!right){
+			rotation_y = -rotation_y;
+		}
+	}
+	Vector3 v = new Vector3 (rotation_x*180/Mathf.PI,rotation_y*180/Mathf.PI,rotation_z*180/Mathf.PI);
     return v;
   }
 
