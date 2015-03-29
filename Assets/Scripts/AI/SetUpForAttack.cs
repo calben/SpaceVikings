63c71b7012d8e8206a574b4f15a1b355dd4d68d3
Calem Bendell <calem.j.bendell@gmail.com>
2015-03-29 02:10:35 -0400

    fixed compiler errors in setupforattack

--- Assets/Scripts/AI/SetUpForAttack.cs
+++ Assets/Scripts/AI/SetUpForAttack.cs
@@ -36,48 +36,59 @@ public class SetUpForAttack : MonoBehaviour
 
   Vector3 MakeGoalPositionFromPlayerPosition(Vector3 playerPosition)
   {
-	Vector3 diff = playerPosition - this.gameObject.transform.position;
-	if (diff.magnitude < 25) {
-		return this.gameObject.transform.position;
-	} else {
-		Vector3 goal = playerPosition + diff.normalized * (diff.magnitude - 25);
-		return goal;
-	}
+    Vector3 diff = playerPosition - this.gameObject.transform.position;
+    if (diff.magnitude < 25)
+    {
+      return this.gameObject.transform.position;
+    }
+    else
+    {
+      Vector3 goal = playerPosition + diff.normalized * (diff.magnitude - 25);
+      return goal;
+    }
   }
 
   Vector3 MakeGoalForwardFromPlayerPosition(Vector3 playerPosition)
   {
-	float rotation_x = 0;
-	float rotation_y = 0;
-	float rotation_z = 0;
-	Vector3 diff = playerPosition - this.goalPosition;
-	float cosphi = Vector3.Dot (this.gameObject.transform.up,diff.normalized, diff.normalized);
-	float costheta = Vector3.Dot (this.gameObject.transform.forward,diff.normalized);
-	if (Math.Abs (cosphi) > .5) {
-		bool right = diff.z > 0;
-		if (cosphi > 0 ){
-			rotation_x = Mathf.PI/3 - Mathf.Acos(cosphi);
-		}
-		else{
-			rotation_x = Mathf.PI/3*2 - Mathf.Acos(cosphi);
-		}
-		if (right){
-			rotation_x = -rotation_x;
-		}
-	}
-	if (Math.Abs (costheta) > .5) {
-		bool right = diff.z > 0;
-		if (costheta > 0 ){
-			rotation_y = Mathf.PI/3 - Mathf.Acos(costheta);
-		}
-		else{
-			rotation_y = Mathf.PI/3*2 - Mathf.Acos(costheta);
-		}
-		if (!right){
-			rotation_y = -rotation_y;
-		}
-	}
-	Vector3 v = new Vector3 (rotation_x*180/Mathf.PI,rotation_y*180/Mathf.PI,rotation_z*180/Mathf.PI);
+    float rotation_x = 0;
+    float rotation_y = 0;
+    float rotation_z = 0;
+    Vector3 diff = playerPosition - this.goalPosition;
+    float cosphi = Vector3.Dot(this.gameObject.transform.up, diff.normalized);
+    float costheta = Vector3.Dot(this.gameObject.transform.forward, diff.normalized);
+    if (Mathf.Abs(cosphi) > .5)
+    {
+      bool right = diff.z > 0;
+      if (cosphi > 0)
+      {
+        rotation_x = Mathf.PI / 3 - Mathf.Acos(cosphi);
+      }
+      else
+      {
+        rotation_x = Mathf.PI / 3 * 2 - Mathf.Acos(cosphi);
+      }
+      if (right)
+      {
+        rotation_x = -rotation_x;
+      }
+    }
+    if (Mathf.Abs(costheta) > .5)
+    {
+      bool right = diff.z > 0;
+      if (costheta > 0)
+      {
+        rotation_y = Mathf.PI / 3 - Mathf.Acos(costheta);
+      }
+      else
+      {
+        rotation_y = Mathf.PI / 3 * 2 - Mathf.Acos(costheta);
+      }
+      if (!right)
+      {
+        rotation_y = -rotation_y;
+      }
+    }
+    Vector3 v = new Vector3(rotation_x * 180 / Mathf.PI, rotation_y * 180 / Mathf.PI, rotation_z * 180 / Mathf.PI);
     return v;
   }
 
--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -4,16 +4,38 @@ using System.Collections;
 public class PlayerHealth : MonoBehaviour
 {
 
-    public float health = 100.0f;
-    public float damageMultiplier = 1.0f;
-    public bool debug = true;
+  public float health = 100.0f;
+  public float damageMultiplier = 1.0f;
+  public bool debug = true;
 
-    void OnCollisionEnter(Collision collision)
+
+  public Texture2D bgImage;
+  public Texture2D fgImage;
+
+  public float healthBarLength;
+
+  void Start()
+  {
+    healthBarLength = Screen.width / 3;
+  }
+
+  void OnGUI()
+  {
+    healthBarLength = (Screen.width / 3) * (this.health / (float)100);
+    GUI.BeginGroup(new Rect(0, 0, healthBarLength, 32));
+    GUI.Box(new Rect(0, 0, healthBarLength, 32), bgImage);
+    GUI.BeginGroup(new Rect(0, 0, this.health / 100 * healthBarLength, 32));
+    GUI.Box(new Rect(0, 0, healthBarLength, 32), fgImage);
+    GUI.EndGroup();
+    GUI.EndGroup();
+  }
+  void OnCollisionEnter(Collision collision)
+  {
+    if (debug)
     {
-        if (debug)
-        {
-            Debug.Log("Player hit collider with magnitude of " + collision.relativeVelocity.magnitude);
-        }
-        this.health -= collision.relativeVelocity.magnitude;
+      Debug.Log("Player hit collider with magnitude of " + collision.relativeVelocity.magnitude);
     }
+    this.health -= collision.relativeVelocity.magnitude;
+  }
+
 }
