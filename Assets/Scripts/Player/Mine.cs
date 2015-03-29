ec66bb9e56292fd3c47a8b5c1526c401a32c7d4f
Calem Bendell <calem.j.bendell@gmail.com>
2015-03-28 23:51:51 -0400

    added player health

--- Assets/Scripts/Player/Mine.cs
+++ Assets/Scripts/Player/Mine.cs
@@ -1,15 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class DetonateMine : MonoBehaviour {
+public class Mine : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+	void OnCollisionEnter()
+  {
+
+  }
 }
