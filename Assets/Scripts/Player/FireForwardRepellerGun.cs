ad1fb9765800c7158a24d751d4f1324acf86edcd
Calem Bendell <calem.j.bendell@gmail.com>
2015-04-17 15:43:17 -0400

    modifications for presentation

---
+++ Assets/Scripts/Player/FireForwardRepellerGun.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireForwardRepellerGun : MonoBehaviour
+{
+
+  public float forwardGunCooldown = 10.0f;
+  public float forwardGunCooldownTmp = 10.0f;
+  public GameObject gravityGunProjectile;
+  public float distanceFromPlayer = 50f;
+
+  void FireGun()
+  {
+    GameObject sphere = (GameObject)Instantiate(gravityGunProjectile, this.transform.position + this.distanceFromPlayer * this.transform.forward, Quaternion.identity);
+  }
+
+  void Update()
+  {
+    this.forwardGunCooldownTmp += Time.deltaTime;
+    if (Input.GetKey(KeyCode.G))
+    {
+      if (this.forwardGunCooldownTmp > this.forwardGunCooldown)
+      {
+        FireGun();
+        this.forwardGunCooldownTmp = 0;
+      }
+    }
+  }
+}
